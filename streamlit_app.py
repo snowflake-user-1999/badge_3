5a8adecb402e7470b19312ba6ab1fd65dd780cb6
snowflake-user-1999 <110551589+snowflake-user-1999@users.noreply.github.com>
2022-08-03 13:54:07 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-
+import pandas
 streamlit.title("My Parents New Healthy Diner")
 
 streamlit.header("Breakfast Favorites")
@@ -10,6 +10,6 @@ streamlit.text("🥑🍞 Avocado Toast")
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import Pandas
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
